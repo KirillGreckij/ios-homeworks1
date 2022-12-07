8ae3c483d07182ef8fcc80c8bb78e8b7309ed873
Kirill Greckij <kirill.greckij@yandex.ru>
2022-12-07 10:48:58 +0300

    add project

---
+++ Navigation/FeedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewController.swift
+//  Navigation
+//
+//  Created by Кирилл Грецкий on 07.12.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+}
---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PostViewController.swift
+//  Navigation
+//
+//  Created by Кирилл Грецкий on 07.12.2022.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+    
+}
