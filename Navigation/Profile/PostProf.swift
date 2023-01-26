fd3c1112d0d859d2f94973a603b6471bc65e1e8b
Kirill Greckij <kirill.greckij@yandex.ru>
2023-01-26 11:42:30 +0300

    do homework

---
+++ Navigation/Profile/PostProf.swift
@@ -0,0 +1,8 @@
+//
+//  PostProf.swift
+//  Navigation
+//
+//  Created by Кирилл Грецкий on 26.01.2023.
+//
+
+import Foundation
---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PostTableViewCell.swift
+//  Navigation
+//
+//  Created by Кирилл Грецкий on 26.01.2023.
+//
+
+import Foundation
